6303235c25ce005b936d81e4e8ece16060b1dbe6
vihanking <bakkijobs@gmail.com>
2022-12-25 17:07:45 +0530

    Added new API for get User by firstName

--- src/main/java/com/adp/userservice/repository/UserRepository.java
+++ src/main/java/com/adp/userservice/repository/UserRepository.java
@@ -2,8 +2,14 @@ package com.adp.userservice.repository;
 
 import com.adp.userservice.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity,String> {
+
+    @Query(value = "SELECT * FROM USERS WHERE FIRSTNAME=?1",nativeQuery = true)
+    List<UserEntity> getUsersByFirstName(String firstName);
 }
